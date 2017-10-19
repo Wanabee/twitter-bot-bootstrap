ce43ec817f7f687b056d94af17c02f2bb85f1b24
maksim <maxime@freddelacompta.com>
2017-10-19 11:05:33 +0200

    grrrrr

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ module.exports = {
     resultType: process.env.RESULT_TYPE,
     language: process.env.LANGUAGE,
     username: process.env.TWITTER_USERNAME,
-    tweetInterval: process.env.TWITTER_TWEET_RATE,
+    tweetInterval: process.env.TWITTER_TWEET_RATE * 60,
     retweet: process.env.TWITTER_RETWEET_RATE * 1000 * 60,
     like: process.env.TWITTER_LIKE_RATE * 1000 * 60,
     quote: process.env.TWITTER_QUOTE_RATE * 1000 * 60,
